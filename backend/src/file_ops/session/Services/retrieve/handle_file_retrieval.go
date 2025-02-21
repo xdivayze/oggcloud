8a67be29e5f5dc51ffb7b13daa0f089b71f74090
xdivayze <ozangencay123@gmail.com>
2025-02-21 21:46:25 +0000

    implemented additional method for getting owner file id from preview id

--- backend/src/file_ops/session/Services/retrieve/handle_file_retrieval.go
+++ backend/src/file_ops/session/Services/retrieve/handle_file_retrieval.go
@@ -1,12 +1,14 @@
 package retrieve
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"oggcloudserver/src/db"
 	"oggcloudserver/src/file_ops/file"
 	"oggcloudserver/src/user/model"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -15,7 +17,38 @@ import (
 const FILE_ID_FIELD = "fileID"
 const PULL_METHOD_FIELD = "pullMethod"
 
-func HandleRetrieve(c *gin.Context) { //work with offset get requests, not multiple photos at one request or ID
+func GetOwnerFileIDFromPreviewID(c *gin.Context) {
+	foundFile := &file.File{}
+	foundUser := &model.User{}
+
+	email := c.Request.Header.Get(model.EMAIL_FIELDNAME)
+	if err := db.DB.Where("email = ?", email).Find(foundUser).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		fmt.Fprintf(os.Stderr, "error occured while finding user with email %s:\n\t%v\n", email, err)
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	prID := c.GetHeader(FILE_ID_FIELD)
+	if prID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "fileID field doesn't exist in the header"})
+		return
+	}
+	if err := db.DB.Where("preview_id = ?", prID).Where("user_id = ?", foundUser.ID).Find(&foundFile).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		c.Status(http.StatusInternalServerError)
+		fmt.Fprintf(os.Stderr, "error occured while finding file with preview_id %s\n\t%v\n", prID, err)
+		return
+	}
+	c.JSON(http.StatusFound, gin.H{"id": foundFile.ID})
+}
+
+func HandleRetrieve(c *gin.Context) { //work with offset or ID get requests, not multiple photos at one request
 	log.SetPrefix("ERR: ")
 	returnedFile := &file.File{}
 	if c.Request.Header.Get(PULL_METHOD_FIELD) == "offset" {
@@ -55,7 +88,7 @@ func HandleRetrieve(c *gin.Context) { //work with offset get requests, not multi
 		return
 	}
 
-	if err := doLoadFileAndStream(c, returnedFile); err != nil { 
+	if err := doLoadFileAndStream(c, returnedFile); err != nil {
 		log.Printf("error occured while loading and streaming file:\n\t%v\n ", err)
 		return
 	}
